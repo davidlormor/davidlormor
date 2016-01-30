baa4169a607209289be857db0525fdde204f2a24
David Lormor <dlormor@cayennecreative.com>
2016-01-30 17:25:42 -0600

    Animate main navigation

--- app/components/ui/main-nav/component.js
+++ app/components/ui/main-nav/component.js
@@ -2,5 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'nav',
-  classNames: ['ui', 'main-nav']
+  classNames: ['ui', 'main-nav'],
+  classNameBindings: ['isOpen'],
+  
+  isOpen: false,
+  
+  mouseEnter() {
+    this.set('isOpen', true);
+  },
+  
+  mouseLeave() {
+    this.set('isOpen', false);
+  }
 });
